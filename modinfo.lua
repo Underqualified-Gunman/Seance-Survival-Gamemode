14e6211e5da238b166c977feff9b348819f3ecf0
Underqualified-Gunman <ghostboy21134@gmail.com>
2024-09-21 13:44:57 -0500

    Basic Mod Prefab
    very basic mod info

---
+++ modinfo.lua
@@ -0,0 +1,11 @@
+name = "Seance Survival Gamemode"
+description = "This mod adds a new gamemode focused character roster mechanics for smaller coop groups. Acts Somewhere between Endless, Wilderness, and Survival"
+author = "Underqualified Gunman"
+version = "alpha 1"
+api_version = 10
+dst_compatible = true
+all_clients_require_mod = true
+client_only_mod = false
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
